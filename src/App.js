9633eaa39021f078b61454779f62cf5a70fca4a5
yuanmanrong <1638620703@qq.com>
2021-09-07 14:31:58 +0800

    username test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@
 import Vote from "../src/component/Vote";
 import Life from "../src/component/LifeCycle";
 
+import { createStore } from "redux";
+
+let state = {
+  n: 1,
+  m: 2,
+};
+let reducer = (state, action) => {}; //修改状态
+
+let store = createStore(reducer); //创建容器需要把reducer传递进来，这里登记了所有状态更改的信息。
+
 function App() {
   return (
     <div>
